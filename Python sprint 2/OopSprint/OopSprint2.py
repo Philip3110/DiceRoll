ef3eecd0b00d19d9ac20dc5e42394956871e9cad
Philip3110 <62377349+Philip3110@users.noreply.github.com>
2020-06-12 09:55:29 +0200

    Add files via upload

--- Python sprint 2/OopSprint/OopSprint2.py
+++ Python sprint 2/OopSprint/OopSprint2.py
@@ -1,25 +1,26 @@
 import random
 
 
-class dice:
+class dice:  # class created
     num = 0
 
     def roll():
-        num = random.randint(1, 6)
+        num = random.randint(1, 6)  # get random number
         return num
 
 
 game_run = True
+print('Welcome to this fun rolling game!')
 
 while game_run:
     game = input('Do you wanna roll a dice? type y/n')
-    if game[0] == 'y':
+    if game[0] == 'y':  # check if player wants to play by checking the input
         d1 = dice
         d2 = dice
 
-        num1 = d1.roll()
+        num1 = d1.roll()  # make a roll of the dice
         num2 = d2.roll()
-        print(str(num1))
+        print(str(num1))  # get the value of num1
         print(str(num2))
         print('Total of both dices is:' + str(num1 + num2))
     else:
--- Python sprint 2/spint2.py
+++ Python sprint 2/spint2.py
@@ -1,19 +1,21 @@
 import random
 
-firstdice = 0
+firstdice = 0  # set the value to 0
 secdic = 0
 
 game_run = True
+print('Welcome do the roll dice game!')
 
 while game_run:
     game = input('Do you wanna roll a dice? type y/n')
-    if game[0] == 'y':
+    if game[0] == 'y':  # checking if the input is 'yes or y'
         game_run = True
-        firstdice = random.randint(1, 6)
+        firstdice = random.randint(1, 6)  # get a random nuber
         secdic = random.randint(1, 6)
         print('The number on first dice is: ' + str(firstdice))
         print('The number on Seconf dice is: ' + str(secdic))
 
+        # add the two dices and get a total
         print('The total of all the dices is: '+str(firstdice + secdic))
     else:
         print('Thank you for rolling the dice')
